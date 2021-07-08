b079de42bf5609a1df842a77367b1f97c5a8eaf7
Kelvin Bush <kelybush@gmail.com>
2021-07-08 13:36:30 +0300

    adding network image
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text("Hello World"),
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Image(
+            image: NetworkImage(
+                'https://i.natgeofe.com/n/d472dd3c-8d38-4eed-ae62-7472a12a17de/secretary-bird-thumbnail-nationalgeographic_2331336.jpg'),
+          ),
+        ),
+      ),
+    );
